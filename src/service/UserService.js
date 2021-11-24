00dbb8820dd0f6945ea2101dfb8be53a7c8713dc
Jordy Coder <59865566+darksos34@users.noreply.github.com>
2021-11-24 09:19:26 +0100

    Http-common

---
+++ src/service/UserService.js
@@ -0,0 +1,31 @@
+import http from '../http-common';
+
+/* All rights reserved by Code Network */
+
+// Get all users API HTTP request handler
+const getAll = async () => {
+    return http.get('/users');
+}
+
+// Get single user by ID API HTTP request handler
+const get = (userId) => {
+    return http.get(`/users/${userId}`);
+}
+
+// Update user API HTTP request handler
+const update = (user) => {
+    return http.put('/users', user);
+}
+
+// Delete user API HTTP request handler
+const remove = (userId) => {
+    return http.delete(`/users/${userId}`);
+}
+
+// Make methods available to import in other components
+export default {
+    getAll,
+    get,
+    update,
+    remove
+}
